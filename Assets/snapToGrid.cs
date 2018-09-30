a61e8aca167d983dd0a0cada8c6e4da5c4d16dd7
Carl Spidla <carlospidla@gmail.com>
2018-09-30 17:19:25 -0400

    'Carl Scene Sept 30' has 	- named and ordered grid nodes with labels 						- mirrorparrent prefab snaps to grid, coordinate assigned in inspector

---
+++ Assets/snapToGrid.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class snapToGrid : MonoBehaviour {
+
+
+    public string mirrorPosition;
+    GameObject foundPosition;
+
+	void Update () {
+        if (GameObject.Find(mirrorPosition) != null)  //If we input a value
+        {             
+            foundPosition = GameObject.Find(mirrorPosition);            //Find The Object with the coordinate we enter
+            if (foundPosition != null)
+            {
+                if (foundPosition.transform.position != transform.position)     //If it's not alread been moved                                                                              //print("HAS VALUE");
+                    transform.position = foundPosition.transform.position;      //Move to that position
+            }
+        }
+	}
+}
